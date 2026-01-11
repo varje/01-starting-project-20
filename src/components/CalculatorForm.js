c891874d38ed42d31cba6f9b14ccdd177ac2213f
varje <varjekivila@gmail.com>
2026-01-11 13:37:23 +0200

    add userInput dummy data; add submit logic

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,6 +1,44 @@
 const CalculatorForm = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const userInput = {
+      currentSavings: 100,
+      yearlyContribution: 200,
+      expectedReturn: 10,
+      duration: 5,
+    };
+    calculateHandler(userInput);
+  };
+
+  const calculateHandler = (userInput) => {
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput.currentSavings; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput.yearlyContribution; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput.expectedReturn/ 100;
+    const duration = +userInput.duration;
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+
+    // do something with yearlyData ...
+    console.log(yearlyData);
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
